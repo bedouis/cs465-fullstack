32d5dd612ae6adb7f148a421f1b2f1d7e7f771d3
Salah Bedoui <sbedoui@Salahs-Mac-Studio.local>
2024-02-08 07:24:03 -0800

    Static HTML to Dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -3,11 +3,14 @@ const fs = require('fs');
 const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 
+
 /* GET travel view */
 const travel = (req, res) => {
+    console.log('Rendering travel view with trips:', trips);
     res.render('travel', { title: 'Travlr Getaways', trips });
 };
 
 module.exports = {
     travel
 };
+
